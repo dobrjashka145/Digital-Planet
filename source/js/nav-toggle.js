4e78186555b809c8ae8424433dd7d63527fc9ef7
dobrjashka145 <dobrjashka145@gmail.com>
2023-01-26 22:34:36 +0200

    Скрипт открытия меню расширен на меню внутренних страниц

--- source/js/nav-toggle.js
+++ source/js/nav-toggle.js
@@ -2,9 +2,17 @@ const navSite = document.querySelector('.main-nav');
 const contactsHeader = document.querySelector('.page-header__contacts')
 const navToggle = document.querySelector('.page-header__nav-toggle');
 
+const navSiteInner = document.querySelector('.main-nav-inner');
+const contactsHeaderInner = document.querySelector('.page-header__contacts-inner')
+const navToggleInner = document.querySelector('.page-header__nav-toggle-inner');
+
 navSite.classList.remove('main-nav--nojs');
 contactsHeader.classList.remove('page-header__contacts--nojs');
 navToggle.classList.remove('page-header__nav-toggle--nojs');
+
+navSiteInner.classList.remove('main-nav-inner--nojs');
+contactsHeaderInner.classList.remove('page-header__contacts-inner--nojs');
+navToggleInner.classList.remove('page-header__nav-toggle-inner--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navSite.classList.contains('main-nav--closed')) {
@@ -21,5 +29,23 @@ navToggle.addEventListener('click', function () {
     contactsHeader.classList.remove('page-header__contacts--opened');
     navToggle.classList.add('page-header__nav-toggle--closed');
     navToggle.classList.remove('page-header__nav-toggle--opened');
+  }
+});
+
+navToggleInner.addEventListener('click', function () {
+  if (navSiteInner.classList.contains('main-nav-inner--closed')) {
+    navSiteInner.classList.remove('main-nav-inner--closed');
+    navSiteInner.classList.add('main-nav-inner--opened');
+    contactsHeaderInner.classList.remove('page-header__contacts-inner--closed');
+    contactsHeaderInner.classList.add('page-header__contacts-inner--opened');
+    navToggleInner.classList.remove('page-header__nav-toggle-inner--closed');
+    navToggleInner.classList.add('page-header__nav-toggle-inner--opened');
+  } else {
+    navSiteInner.classList.add('main-nav-inner--closed');
+    navSiteInner.classList.remove('main-nav-inner--opened');
+    contactsHeaderInner.classList.add('page-header__contacts-inner--closed');
+    contactsHeaderInner.classList.remove('page-header__contacts-inner--opened');
+    navToggleInner.classList.add('page-header__nav-toggle-inner--closed');
+    navToggleInner.classList.remove('page-header__nav-toggle-inner--opened');
   }
 });
