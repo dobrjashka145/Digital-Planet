9b9c1e1834a0c64b18b4b46247485faf28df44eb
dobrjashka145 <dobrjashka145@gmail.com>
2023-03-22 21:05:15 +0200

    Добавлены фавиконки, верстка модального окна c -nojs, исправлен modal-admin.js

--- source/js/modal-admin.js
+++ source/js/modal-admin.js
@@ -1,36 +1,48 @@
-const orderButtons = document.querySelectorAll('[name="order"]');
+const orderButtons = document.querySelectorAll('[class*="__booking"]');
 const orderCard = document.querySelector('.order-card-container');
-const orderConfirm = document.querySelector('.order-confirm-container');
-const orderSubmit = document.querySelector('.order-form__submit');
 const orderCardToggle = document.querySelector('.order-card__toggle');
+
+const orderConfirm = document.querySelector('.order-confirm-container');
 const orderConfirmToggle = document.querySelector('.order-confirm__toggle');
 const orderConfirmOK = document.querySelector('.order-confirm__button');
 
+const orderForm = document.querySelector('.order-form');
+const orderName = orderForm.querySelector('input[name="order-name"]');
+const orderTel = orderForm.querySelector('input[name="order-tel"]');
+const orderSubmit = document.querySelector('.order-form__submit');
+
 for (let i = 0; i < orderButtons.length; i++) {
-  orderButtons[i].addEventListener('click', function() {
-    if(orderCard.classList.contains('modal-closed')) {
-      orderCard.classList.remove('modal-closed');
+  orderButtons[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    if(orderCard.classList.contains('modal-opened')) {
+      orderCard.classList.remove('modal-opened');
+    } else {
+      orderCard.classList.add('modal-opened');
+      orderName.value = '';
+      orderTel.value = '';
     }
   });
 }
 
 orderSubmit.addEventListener('click', function() {
-  if(orderConfirm.classList.contains('modal-closed')) {
-    orderConfirm.classList.remove('modal-closed');
-    orderCard.classList.add('modal-closed');
+  if(orderConfirm.classList.contains('modal-opened')) {
+    orderConfirm.classList.remove('modal-opened')
+  } else {
+    orderConfirm.classList.add('modal-opened');
+    orderCard.classList.remove('modal-opened');
   }
 });
 
 orderCardToggle.addEventListener('click', function() {
-  orderCard.classList.add('modal-closed');
+  orderCard.classList.remove('modal-opened');
 });
 
 orderConfirmToggle.addEventListener('click', function() {
-  orderConfirm.classList.add('modal-closed');
+  orderConfirm.classList.remove('modal-opened');
 });
 
 orderConfirmOK.addEventListener('click', function() {
-  orderConfirm.classList.add('modal-closed');
+  orderConfirm.classList.remove('modal-opened');
 });
 
 
